9bd7a3f65fdf9e12f12ec3a1a1e12caab9805bc0
Lucas Ferreira <ferreiralucas090@gmail.com>
2023-03-12 14:02:31 -0300

    feat: add navbar

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
-import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(
